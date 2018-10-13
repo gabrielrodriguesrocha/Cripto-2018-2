79a0ef3f50db611117833f8625ac9d0c3dc3e9f7
hskodama <hskodama@hotmail.com>
2018-10-13 15:30:16 -0300

    Implementation of des(), in process of verifying the generation of keys

--- DES/des.c
+++ DES/des.c
@@ -26,7 +26,7 @@ void generate_keys(unsigned long long key) {
       L = L | (((key >> (64 - PC1L[i])) & 1) << (28 - i - 1));
       R = R | (((key >> (64 - PC1R[i])) & 1) << (28 - i - 1));
   }
-  
+
   for (i = 0; i < 16; i++) {
       L = (L << KSROT[i] | L >> (28 - KSROT[i])) & 0xfffffff;
       R = (R << KSROT[i] | R >> (28 - KSROT[i])) & 0xfffffff;
@@ -37,34 +37,34 @@ void generate_keys(unsigned long long key) {
 /* ---- Feistel Function --- */
 // Expands the input and does 8 rounds of S-box permutations
 unsigned int f(unsigned int R, unsigned long long K) {
-    unsigned int res;
-    unsigned int S[9];
+    unsigned long long res;
+    unsigned long long S[9];
 
     res = general_permutation((unsigned long long) R, E, 32, 48);
     res = res ^ K;
 
-    S[8] = res & 0x3f
-    S[7] = res >> 6  & 0x3f
-    S[6] = res >> 12 & 0x3f
-    S[5] = res >> 18 & 0x3f
-    S[4] = res >> 24 & 0x3f
-    S[3] = res >> 30 & 0x3f
-    S[2] = res >> 36 & 0x3f
-    S[1] = res >> 42 & 0x3f
+    S[8] = res & 0x3f;
+    S[7] = res >> 6  & 0x3f;
+    S[6] = res >> 12 & 0x3f;
+    S[5] = res >> 18 & 0x3f;
+    S[4] = res >> 24 & 0x3f;
+    S[3] = res >> 30 & 0x3f;
+    S[2] = res >> 36 & 0x3f;
+    S[1] = res >> 42 & 0x3f;
 
-    S[8] = sbox8[((S[8] >> 5) << 1) & (S[8] & 0x1)][(S[8] >> 1) & 0xf];
-    S[7] = sbox7[((S[7] >> 5) << 1) & (S[8] & 0x1)][(S[7] >> 1) & 0xf];
-    S[6] = sbox6[((S[6] >> 5) << 1) & (S[8] & 0x1)][(S[6] >> 1) & 0xf];
-    S[5] = sbox5[((S[5] >> 5) << 1) & (S[8] & 0x1)][(S[5] >> 1) & 0xf];
-    S[4] = sbox4[((S[4] >> 5) << 1) & (S[8] & 0x1)][(S[4] >> 1) & 0xf];
-    S[3] = sbox3[((S[3] >> 5) << 1) & (S[8] & 0x1)][(S[3] >> 1) & 0xf];
-    S[2] = sbox2[((S[2] >> 5) << 1) & (S[8] & 0x1)][(S[2] >> 1) & 0xf];
-    S[1] = sbox1[((S[1] >> 5) << 1) & (S[8] & 0x1)][(S[1] >> 1) & 0xf];
+    S[8] = sbox8[(((S[8] >> 5) << 1) | (S[8] & 0x1)) * 16 + ((S[8] >> 1) & 0xf)];
+    S[7] = sbox7[(((S[7] >> 5) << 1) | (S[7] & 0x1)) * 16 + ((S[7] >> 1) & 0xf)];
+    S[6] = sbox6[(((S[6] >> 5) << 1) | (S[6] & 0x1)) * 16 + ((S[6] >> 1) & 0xf)];
+    S[5] = sbox5[(((S[5] >> 5) << 1) | (S[5] & 0x1)) * 16 + ((S[5] >> 1) & 0xf)];
+    S[4] = sbox4[(((S[4] >> 5) << 1) | (S[4] & 0x1)) * 16 + ((S[4] >> 1) & 0xf)];
+    S[3] = sbox3[(((S[3] >> 5) << 1) | (S[3] & 0x1)) * 16 + ((S[3] >> 1) & 0xf)];
+    S[2] = sbox2[(((S[2] >> 5) << 1) | (S[2] & 0x1)) * 16 + ((S[2] >> 1) & 0xf)];
+    S[1] = sbox1[(((S[1] >> 5) << 1) | (S[1] & 0x1)) * 16 + ((S[1] >> 1) & 0xf)];
 
-    res = (S[1] << 42) & (S[2] << 36) & (S[3] << 30) & (S[4] << 24) &
-          (S[5] << 18) & (S[6] << 12) & (S[7] <<  6) & (S[8]);
+    res = (S[1] << 42) | (S[2] << 36) | (S[3] << 30) | (S[4] << 24) |
+          (S[5] << 18) | (S[6] << 12) | (S[7] <<  6) | (S[8]);
 
-    res = (unsigned int)general_permutation((unsigned long long)res, P, 32, 32);
+    res = general_permutation((unsigned long long)res, P, 32, 32);
 
     return res;
 }
@@ -72,6 +72,28 @@ unsigned int f(unsigned int R, unsigned long long K) {
 /* ---- DES Cipher --- */
 // Runs through 16 rounds of ciphering action
 unsigned long long des(unsigned long long input) {
+    unsigned long long right, left, aux;
+    int i;
+    printf("Raw input: %llX\n", input);
+    /* ---- Initial Permutation ---- */
+    input = general_permutation(input, IP, 64, 64);
+    printf("Initial Permutation: %llX\n", input);
+    /* ---- Feistel Network ---- */
+    left  = input >> 32 & 0xffffffff;
+    right = input       & 0xffffffff;
+    printf("Left 0: %llX\n", left);
+    printf("Right 0: %llX\n\n", right);
+    for(i = 0 ; i < 16 ; i ++){
+      printf("R%02d = %08llX / SK%02d = %08llX\n", i, right, i+1, keys[i]);
+      aux   = left;
+      left  = right;
+      right = aux ^ f(right, keys[i]);
+      printf("Round %02d: %08llX\n", i + 1, right);
+    }
+
+    /* ---- Final Permutation ---- */
+    input = right << 32 | left;
+    input = general_permutation(input, IPinverse, 64, 64);
     return input;
 }
 
@@ -92,12 +114,7 @@ int main (int argc,char *argv[]) {
     generate_keys(key);
 
      while (fscanf(fp, "%llx", &buffer) != EOF) {
-       /* ---- Initial Permutation ---- */
-       buffer = general_permutation(buffer, IP, 64, 64);
-
-       printf("%llx\n", des(buffer));
-
-       /* ---- Final Permutation ---- */
+       printf("%llX\n", des(buffer));
      }
 
     return 0;
